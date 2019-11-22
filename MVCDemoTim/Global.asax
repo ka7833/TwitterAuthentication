38d19ceecb612967e60e89de4fce5768158b591c
Kaleab Admassu <Kadmassu@lazparking.com>
2019-11-21 22:37:46 -0500

    Add project files.

---
+++ MVCDemoTim/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDemoTim.MvcApplication" Language="C#" %>
---
+++ MVCDemoTim/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCDemoTim.Startup))]
+namespace MVCDemoTim
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
